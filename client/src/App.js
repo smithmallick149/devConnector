cf479d599b0164ec73ea5e9081a9f70de8b2fc13
smith mallick <smith@ariveguru.com>
2020-04-19 22:46:04 +0530

    react with redux implementation done

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,36 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import './App.css';
-import NavBar from './components/layout/NavBar';
 import Landing from './components/layout/Landing';
-import Login from './components/auth/Login';
-import Register from './components/auth/Register';
+import Routes from './components/routing/Routes';
+import Navbar from './components/layout/Navbar';
 
-const App = () => (
-	<Router>
-		<Fragment>
-			<NavBar />
-			<Route exact path="/" component={Landing} />
-			<section className="container">
-				<Switch>
-					<Route exact path="/register" component={Register} />
-					<Route exact path="/login" component={Login} />
-				</Switch>
-			</section>
-		</Fragment>
-	</Router>
-);
+// Redux
+import { Provider } from 'react-redux';
+import store from './store';
+import { loadUser } from './actions/auth';
+import setAuthToken from './utils/setAuthToken';
+
+import './App.css';
+
+const App = () => {
+	useEffect(() => {
+		setAuthToken(localStorage.token);
+		store.dispatch(loadUser());
+	}, []);
+
+	return (
+		<Provider store={store}>
+			<Router>
+				<Fragment>
+					<Navbar />
+					<Switch>
+						<Route exact path="/" component={Landing} />
+						<Route component={Routes} />
+					</Switch>
+				</Fragment>
+			</Router>
+		</Provider>
+	);
+};
 
 export default App;
